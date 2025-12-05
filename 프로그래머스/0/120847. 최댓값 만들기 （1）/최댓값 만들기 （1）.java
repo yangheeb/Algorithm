5ea1b440360643a317682ff878ee9cf21a4414ff
백민정 <110668121+yangheeb@users.noreply.github.com>
2025-12-05 19:24:41 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.48 ms, Memory: 84.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        Arrays.sort(numbers);
+        int n = numbers.length;
+        return numbers[n-1] * numbers[n-2];
+    }
+}
